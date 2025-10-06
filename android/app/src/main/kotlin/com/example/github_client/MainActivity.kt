efde2d4d4eb815e31738346fcc665270fcb3ef6d
yeongdolee <yeongdo1018@outlook.com>
2025-10-06 19:48:15 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/github_client/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.github_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'Ov23li26A3CBuyj2ntJP';
+const githubClientSecret = 'ff5c9519aafb1f92772bb87b3b0c42db8b254474';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
